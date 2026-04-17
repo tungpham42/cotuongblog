d76b530b0721441e000e0f78523869d26c686563
Tung Pham <tung.42@gmail.com>
2026-04-17 12:58:04 +0700

    Update sitemap.blade.php

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,20 +1,10 @@
-<?xml version="1.0" encoding="UTF-8"?>
+{!! '<' . '?xml version="1.0" encoding="UTF-8"?' . '>' !!}
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 
     <url>
         <loc>{{ route('home') }}</loc>
         <changefreq>daily</changefreq>
         <priority>1.0</priority>
-    </url>
-    <url>
-        <loc>{{ route('login') }}</loc>
-        <changefreq>monthly</changefreq>
-        <priority>0.5</priority>
-    </url>
-    <url>
-        <loc>{{ route('register') }}</loc>
-        <changefreq>monthly</changefreq>
-        <priority>0.5</priority>
     </url>
 
     @foreach ($posts as $post)
@@ -29,18 +19,8 @@
     @foreach ($categories as $category)
         <url>
             <loc>{{ route('categories.show', $category->slug) }}</loc>
-            <lastmod>{{ $category->updated_at->tz('UTC')->toAtomString() }}</lastmod>
             <changefreq>weekly</changefreq>
             <priority>0.7</priority>
-        </url>
-    @endforeach
-
-    @foreach ($tags as $tag)
-        <url>
-            <loc>{{ route('tags.show', $tag->slug) }}</loc>
-            <lastmod>{{ $tag->updated_at->tz('UTC')->toAtomString() }}</lastmod>
-            <changefreq>weekly</changefreq>
-            <priority>0.6</priority>
         </url>
     @endforeach
 
