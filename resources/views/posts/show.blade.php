61d7068200d4ce5b2d1cfd34d4b850f86635d966
Tung Pham <tung.42@gmail.com>
2026-05-13 12:18:36 +0700

    Update show.blade.php

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -51,7 +51,7 @@
     <div class="my-8 w-full overflow-hidden flex justify-center not-prose">
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3585118770961536" crossorigin="anonymous"></script>
         <ins class="adsbygoogle"
-             style="display:block; text-align:center;"
+             style="display:block; width:100%; height:120px; text-align:center;"
              data-ad-layout="in-article"
              data-ad-format="fluid"
              data-ad-client="ca-pub-3585118770961536"
