1276226fa776ab8788307a0d719f04d28000c53b
Tung Pham <tung.42@gmail.com>
2026-08-23 18:27:06 +0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,5 +1,14 @@
 <?php
 
+// --- BẮT ĐẦU FIX SYMLINK ---
+$uri = $_SERVER['REQUEST_URI'] ?? '';
+// Nếu URL bắt đầu bằng /blog, ta cắt bỏ chữ /blog đi để Laravel hiểu đây là trang chủ (/)
+if (strpos($uri, '/blog') === 0) {
+    $newUri = substr($uri, 5);
+    $_SERVER['REQUEST_URI'] = ($newUri === '' || $newUri[0] !== '/') ? '/' . $newUri : $newUri;
+}
+// --- KẾT THÚC FIX SYMLINK ---
+
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
